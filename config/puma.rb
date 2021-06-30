60bcf06590533a45b51821c42d990b9ab8fd9dd9
SergePauli <SergePauli@yandex.ru>
2021-06-30 18:12:31 +0800

    comlited README.md

--- config/puma.rb
+++ config/puma.rb
@@ -18,8 +18,8 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+# default is production (test production development)
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
